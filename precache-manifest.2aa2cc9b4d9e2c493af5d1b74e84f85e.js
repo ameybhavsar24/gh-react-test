be13a00ec5434db5563ce4f38fc17b8a3cd7506f
Amey Bhavsar <iamamey24@gmail.com>
2020-09-11 23:15:58 +0530

    Updates

---
+++ precache-manifest.2aa2cc9b4d9e2c493af5d1b74e84f85e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86fa2ec73922e6db2ae31cf7b424b137",
+    "url": "/gh-react-test/index.html"
+  },
+  {
+    "revision": "cde2bf7301645531ec11",
+    "url": "/gh-react-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bc66a1e4c5ef967495ee",
+    "url": "/gh-react-test/static/js/2.41e7d550.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh-react-test/static/js/2.41e7d550.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cde2bf7301645531ec11",
+    "url": "/gh-react-test/static/js/main.f9ec0c41.chunk.js"
+  },
+  {
+    "revision": "f96540a89089c773bb88",
+    "url": "/gh-react-test/static/js/runtime-main.f3ee870b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-react-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
